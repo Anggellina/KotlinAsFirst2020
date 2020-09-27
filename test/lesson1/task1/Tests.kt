0479fd50e2e83b212dfc15244e18c918b66cc446
Anggellina <71499831+Anggellina@users.noreply.github.com>
2020-09-27 17:27:29 +0600

    first task solved

--- test/lesson1/task1/Tests.kt
+++ test/lesson1/task1/Tests.kt
@@ -78,7 +78,7 @@ class Tests {
 
     @Test
     @Tag("2")
-    fun thirdDigit() {
+    fun trackL() {
         assertEquals(8, thirdDigit(3801))
         assertEquals(1, thirdDigit(100))
         assertEquals(0, thirdDigit(1000))
