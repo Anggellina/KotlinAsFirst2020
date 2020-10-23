c68f19e7b03a16d6fabfd51e5c25d4c55cedfe0f
Anggellina <71499831+Anggellina@users.noreply.github.com>
2020-10-23 21:01:30 +0300

    l

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -136,11 +136,9 @@ fun abs(v: List<Double>): Double {
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
 fun mean(list: List<Double>): Double {
-    val k = list.size
+    if (list.size == 0) return 0.0
     val sum = list.sum()
-    if (k == 0) return 0.0
-    return if (k == 1) sum
-    else sum / k
+    return sum / list.size
 }
 
 /**
@@ -198,11 +196,11 @@ fun polynom(p: List<Int>, x: Int): Int = TODO()
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
 fun accumulate(list: MutableList<Int>): MutableList<Int> {
-    var sum = 0.0
+    var sum = 0
     val large = list.size - 1
     for (i in 0..large) {
         sum += list[i]
-        list[i] = sum.toInt()
+        list[i] = sum
     }
     return list
 }
